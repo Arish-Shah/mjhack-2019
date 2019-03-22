46149e5f82b9c5263045c04224ebcc1217b80671
arizh <arish0246@gmail.com>
2019-03-23 00:17:34 +0530

    First Commit

---
+++ public/js/config.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAwabUBCGYYoPRVSPe3Wb35uRtyE4DDRxQ",
+    authDomain: "mjhackrev.firebaseapp.com",
+    databaseURL: "https://mjhackrev.firebaseio.com",
+    projectId: "mjhackrev",
+    storageBucket: "mjhackrev.appspot.com",
+    messagingSenderId: "959088140118"
+  };
+  firebase.initializeApp(config);
